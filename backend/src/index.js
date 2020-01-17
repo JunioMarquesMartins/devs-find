357178e080161523aca50c1202dfea34c2636a10
Junio MM <info@juniomarques.com>
2020-01-17 17:35:19 +0100

    init app

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://juniomarques:jmarquesFront77@cluster0-jghfd.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+app.use(express.json());
+app.use(routes);
+app.listen(3333);
+
+
+
+// Metodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenaçao, paginaçao, ...);
+// Route Params: reuqest.params (Identificar um recurso na añteraçao ou remoçao);
+// Body request.body (Dados para criaçao ou alteraçao de um registro);
+
+// MongoDB (Nao-relacional)
+
+
+
